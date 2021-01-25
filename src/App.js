623a12d3f6c0c27e9f00facb15b0b945a59d9efa
Shah <shahbhumika70@gmail.com>
2021-01-26 01:51:51 +0530

    Creating shopping page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import HomePage from './pages/homepage/homepage'
+import HomePage from './pages/homepage/homepage';
+import ShopPage from './pages/shop/shop';
+import { Route } from 'react-router-dom';
 
 function App() {
   return (
-      <HomePage></HomePage>
+    <div>
+      <switch>
+        <Route exact path='/' component={HomePage}></Route>
+        <Route path='/shop' component={ShopPage}></Route>
+      </switch>
+    </div>
   );
 }
 
